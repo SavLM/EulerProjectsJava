18ba24662f83e5384c0d450f7956bb3a2dc2982e
SavLM <50849597+SavLM@users.noreply.github.com>
2019-05-25 23:09:14 -0400

    Added test files

---
+++ tests/com/savannahmclaurin/Problem1Test.java
@@ -0,0 +1,12 @@
+package com.savannahmclaurin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem1Test {
+
+    @Test
+    public void problem1solution_r() {
+    }
+}
